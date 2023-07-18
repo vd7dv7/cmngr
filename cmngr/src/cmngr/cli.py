f03d2f3137cf3670a1c4ddc2766d443d1af50402
vd7dv7 <139835776+vd7dv7@users.noreply.github.com>
2023-07-17 21:05:34 -0700

    Add files via upload

---
+++ cmngr/src/cmngr/cli.py
@@ -0,0 +1,272 @@
+import json
+import os
+from pathlib import Path
+
+import inquirer
+import typer
+from rich.console import Console
+from rich.prompt import IntPrompt, Prompt, Confirm
+
+from cmngr import config, util
+from cmngr.discord import client
+from cmngr.model import get_all_embeddings, get_all_llms
+from cmngr.settings import Settings, get_all_model_settings, load_model_setting
+from cmngr.cmngr import agi_init, agi_step
+
+cli = typer.Typer()
+console = Console()
+
+#######################################################################################
+# Config CLI
+
+config_cli = typer.Typer()
+
+
+@config_cli.command("openai")
+def config_openai():
+    """
+    Configure OpenAI API token
+    """
+    token = Prompt.ask("Enter your OpenAI API token").strip()
+    verify_resp = util.verify_openai_token(token)
+    if verify_resp != "OK":
+        console.print("[Error] OpenAI Token is invalid", style="red")
+        console.print(verify_resp)
+        return
+    config.set_openai_token(token)
+    console.print("OpenAI Key is Configured Successfully!", style="green")
+
+
+@config_cli.command("pinecone")
+def config_pinecone():
+    """
+    Configure Pinecone API token
+    """
+    token = Prompt.ask("Enter your Pinecone API token").strip()
+    verify_resp = util.verify_pinecone_token(token)
+    if verify_resp != "OK":
+        console.print("[Error] Pinecone Token is invalid", style="red")
+        console.print(verify_resp)
+        return
+    config.set_pinecone_token(token)
+    console.print("Pinecone Key is Configured Successfully!", style="green")
+
+
+@config_cli.command("discord")
+def config_discord():
+    """
+    Configure discord API token
+    """
+    token = Prompt.ask("Enter your Discord API token").strip()
+    verify_resp = util.verify_discord_token(token)
+    if verify_resp != "OK":
+        console.print("[Error] Discord Token is invalid", style="red")
+        console.print(verify_resp)
+        return
+    config.set_discord_token(token)
+    console.print("Discord Key is Configured Successfully!", style="green")
+
+
+@config_cli.callback(invoke_without_command=True)
+def config_main(ctx: typer.Context):
+    """
+    Configure cmngr
+    """
+    # only run without a command specified
+    if ctx.invoked_subcommand is not None:
+        return
+
+    console.print("cmngr's Configuration")
+
+    if config.load_openai_token():
+        console.print("OpenAI Token is configured, good job!", style="green")
+    else:
+        console.print(
+            "OpenAI Token not configured yet! This is necessary to use cmngr",
+            style="red",
+        )
+        console.print("To config OpenAI token: [yellow]cmngr config openai[/yellow]")
+
+    if config.load_pinecone_token():
+        console.print("Pinecone Token is configured, good job!", style="green")
+    else:
+        console.print(
+            "Pinecone Token not configured yet! This is necessary to use cmngr",
+            style="red",
+        )
+        console.print(
+            "To config Pinecone token: [yellow]cmngr config pinecone[/yellow]"
+        )
+
+    if config.load_discord_token():
+        console.print("Discord Token is configured, good job!", style="green")
+    else:
+        console.print("Discord Token not configured yet!", style="red")
+        console.print("To config Discord token: [yellow]cmngr config discord[/yellow]")
+
+
+cli.add_typer(config_cli, name="config")
+
+#######################################################################################
+# Model CLI
+
+model_cli = typer.Typer(help="Models Management")
+
+
+@model_cli.command("list")
+def model_list():
+    """
+    List all supported models
+    """
+    console.print("\n[green]Supported LLM/ChatModel:\n")
+    console.print(", ".join(get_all_llms()))
+    console.print("\n[green]Supported Embedding:\n")
+    console.print(", ".join(get_all_embeddings()))
+
+
+@model_cli.callback(invoke_without_command=True)
+def model_main(ctx: typer.Context):
+    """
+    Models
+    """
+    # only run without a command specified
+    if ctx.invoked_subcommand is not None:
+        return
+
+    # by default just print all supported models
+    model_list()
+
+
+cli.add_typer(model_cli, name="model")
+
+#######################################################################################
+# Main CLI
+
+
+@cli.command("discord")
+def status():
+    """
+    Run the Discord client
+    """
+    # Verify the Discord token before anything else
+    discord_token = config.load_discord_token()
+    verify_discord = util.verify_discord_token(discord_token)
+    if verify_discord != "OK":
+        console.print("Please config your Discord token", style="red")
+        console.print(verify_discord)
+        return
+    console.print("Discord client starting...", style="yellow")
+    client.run(discord_token)
+
+
+@cli.command("run")
+def run():
+    """
+    Run cmngr
+    """
+    settings = Settings()
+
+    # Ask Model settings
+    questions = [
+        inquirer.List(
+            "llm-model",
+            message="What LLM model you want to use?",
+            choices=get_all_model_settings(),
+        )
+    ]
+    answers = inquirer.prompt(questions=questions)
+    settings.model = load_model_setting(answers["llm-model"])
+
+    openai_key = config.load_openai_token()
+    if os.getenv("OPENAI_API_KEY") is None:
+        os.environ["OPENAI_API_KEY"] = openai_key
+
+    # Model initialization verification
+    res = util.verify_model_initialization(settings)
+    if res != "OK":
+        console.print(res, style="red")
+        return
+    # Get inputs from the user
+    agent_count = IntPrompt.ask(
+        "Number of agents to create (at least 2 agents)?", default=3
+    )
+    if agent_count < 2:
+        console.print("Please config at least 2 agents, exiting", style="red")
+        return
+    agent_configs = []
+    agent_names = []
+    for idx in range(agent_count):
+        console.print(f"Specify agent number {idx+1}")
+        agent_config = {}
+        while True:
+            agent_file = Prompt.ask(
+                "Enter the path to the agent configuration file", default="./agent.json"
+            )
+            if not os.path.isfile(agent_file):
+                console.print(f"Invalid file path: {agent_file}", style="red")
+                continue
+            try:
+                agent_config = util.load_json(Path(agent_file))
+                agent_config["path"] = agent_file
+                if agent_config == {}:
+                    console.print(
+                        "Empty configuration, please provide a valid one", style="red"
+                    )
+                    continue
+                break
+            except json.JSONDecodeError:
+                console.print(
+                    "Invalid configuration, please provide a valid one", style="red"
+                )
+                continue
+        agent_configs.append(agent_config)
+        agent_names.append(agent_config["name"])
+
+    user_role = Prompt.ask(
+        "Pick which role you want to perform? (input the exact name, case sensitive)",
+        choices=agent_names,
+        default=agent_names[0],
+    )
+    user_index = agent_names.index(user_role)
+
+    from_checkpoint = Confirm.ask("Load agents' memories from checkpoint?", default=False)
+
+    ctx = agi_init(agent_configs, console, settings, user_index, from_checkpoint=from_checkpoint)
+
+    actions = ["continue", "interview", "exit"]
+    while True:
+        instruction = {"command": "continue"}
+        action = Prompt.ask(
+            "Pick an action to perform?", choices=actions, default=actions[0]
+        )
+        if action == "interview":
+            robot_agent_names = list(map(lambda agent: agent.name, ctx.robot_agents))
+            robot_agent_name = Prompt.ask(
+                f"As {ctx.user_agent.name}, which agent do you want to talk to?",
+                choices=robot_agent_names,
+                default=robot_agent_names[0],
+            )
+            instruction = {
+                "command": "interview",
+                "agent_to_interview": ctx.robot_agents[
+                    robot_agent_names.index(robot_agent_name)
+                ],
+            }
+        elif action == "exit":
+            console.print("cmngr exiting...", style="yellow")
+            break
+        agi_step(ctx, instruction)
+
+
+@cli.callback(invoke_without_command=True)
+def main(
+    ctx: typer.Context,
+):
+    """
+    Default behavior
+    """
+    # only run without a command specified
+    if ctx.invoked_subcommand is not None:
+        return
+
+    run()
---
+++ cmngr/src/cmngr/cmngr.py
@@ -0,0 +1,156 @@
+from typing import List
+
+from rich.console import Console
+
+from cmngr.context import Context
+from cmngr.model import load_llm_from_config
+from cmngr.settings import Settings
+from cmngr.simulation.agent import GenerativeAgent
+from cmngr.simulation.simulation import (
+    create_new_memory_retriever,
+    interview_agent,
+    run_conversation,
+    talks_to,
+)
+from cmngr.util import get_checkpoint_dir
+
+
+def user_robot_conversation(agent_to_interview: GenerativeAgent, ctx: Context):
+    ctx.print(
+        f"Interview with {agent_to_interview.name} start, input empty line to exit",
+        style="yellow",
+    )
+    ctx.observations.append(
+        f"{ctx.user_agent.name} now is having a conversation with {agent_to_interview.name}"
+    )
+    while True:
+        user_message = ctx.ask()
+        if user_message == "":
+            ctx.print(f"Interview with {agent_to_interview.name} finished")
+            break
+        ctx.observations.append(f"{ctx.user_agent.name} said: {user_message}")
+        with ctx.console.status("[yellow]Waiting response..."):
+            response = interview_agent(
+                agent_to_interview, user_message, ctx.user_agent.name
+            )
+        if "GOODBYE" in response:
+            ctx.print(
+                f"{agent_to_interview.name} said Goodbye and ended the conversation"
+            )
+            break
+        ctx.print(response)
+        ctx.observations.append(response)
+    ctx.observations.append(
+        f"The conversation between {ctx.user_agent.name} and {agent_to_interview.name} is ended."
+    )
+
+
+def agi_step(ctx: Context, instruction: dict) -> None:
+    ctx.clock += 1
+    if instruction["command"] == "interview":
+        user_robot_conversation(instruction["agent_to_interview"], ctx)
+
+    if instruction["command"] == "continue":
+        someone_asked = False
+        for robot_agent in ctx.robot_agents:
+            with ctx.console.status("[yellow]Something is going on..."):
+                message = talks_to(robot_agent, ctx.user_agent, ctx.observations)
+            if message:
+                if someone_asked:
+                    ctx.print(
+                        f"{robot_agent.name} also whispered to you({ctx.user_agent.name}): {message}",
+                        style="yellow",
+                    )
+                else:
+                    ctx.print(
+                        f"{robot_agent.name} whispered to you ({ctx.user_agent.name}): {message}",
+                        style="yellow",
+                    )
+                someone_asked = True
+                respond = ctx.ask(
+                    f"Do you want to respond to {robot_agent.name}?",
+                    choices=["yes", "no"],
+                )
+                if respond == "yes":
+                    user_robot_conversation(robot_agent, ctx)
+
+    ctx.print("The world has something else happening...", style="yellow")
+    # let the activities of non user robots happen
+    for idx in range(len(ctx.robot_agents) - 1):
+        amy = ctx.robot_agents[idx]
+        for bob in ctx.robot_agents[idx + 1 :]:
+            message = talks_to(amy, bob, ctx.observations)
+            if message:
+                ctx.print(f"{amy.name} just whispered to {bob.name}...", style="yellow")
+                with ctx.console.status(
+                    f"[yellow]{amy.name} is having a private dicussion with {bob.name}..."
+                ):
+                    run_conversation([amy, bob], f"{amy.name} said: {message}", ctx)
+                ctx.print(
+                    f"{amy.name} and {bob.name} finished their private conversation...",
+                    style="yellow",
+                )
+                continue
+            message = talks_to(bob, amy, ctx.observations)
+            if message:
+                ctx.print(f"{bob.name} just whispered to {amy.name}...", style="yellow")
+                run_conversation([bob, amy], f"{bob.name} said: {message}", ctx)
+                ctx.print(
+                    f"{bob.name} and {amy.name} finished their private conversation...",
+                    style="yellow",
+                )
+                continue
+
+    # clean up context's observations based on the time window
+    ctx.observations_size_history.append(len(ctx.observations))
+    if len(ctx.observations_size_history) > ctx.timewindow_size:
+        remove_count = ctx.observations_size_history[0]
+        ctx.observations = ctx.observations[remove_count:]
+        ctx.observations_size_history = ctx.observations_size_history[1:]
+        for idx in range(len(ctx.observations_size_history)):
+            ctx.observations_size_history[idx] = (
+                ctx.observations_size_history[idx] - remove_count
+            )
+
+
+def agi_init(
+    agent_configs: List[dict],
+    console: Console,
+    settings: Settings,
+    user_idx: int = 0,
+    webcontext=None,
+    from_checkpoint=False
+) -> Context:
+    ctx = Context(console, settings, webcontext)
+    ctx.print("Creating all agents one by one...", style="yellow")
+    for idx, agent_config in enumerate(agent_configs):
+        agent_name = agent_config["name"]
+        with ctx.console.status(f"[yellow]Creating agent {agent_name}..."):
+            agent = GenerativeAgent(
+                name=agent_config["name"],
+                age=agent_config["age"],
+                traits=agent_config["personality"],
+                status="N/A",  # When connected to a virtual world, we can have the characters update their status
+                memory_retriever=create_new_memory_retriever(ctx),
+                llm=load_llm_from_config(ctx.settings.model.llm),
+                daily_summaries=[(agent_config["current_status"])],
+                reflection_threshold=8,
+            )
+            checkpoint_dir = get_checkpoint_dir(agent_config["path"])
+            if from_checkpoint and agent.try_load_memory(checkpoint_dir):
+                pass
+            else:
+                for memory in agent_config["memories"]:
+                    agent.add_memory(memory)
+                agent.dump_memory(checkpoint_dir)
+        if idx == user_idx:
+            ctx.user_agent = agent
+        else:
+            ctx.robot_agents.append(agent)
+        ctx.agents.append(agent)
+        ctx.observations.append(agent_config["current_status"])
+        ctx.print(f"Agent {agent_name} successfully created", style="green")
+
+    ctx.print("cmngr started...")
+    ctx.print(f"You are going to behave as {ctx.user_agent.name}", style="yellow")
+    return ctx
